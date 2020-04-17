76f5f8c57e2ff48200b2ba1992cd32bd13fbba9c
Weixi Zhu <wxzhu@rice.edu>
2020-04-17 14:28:13 -0500

    [need to convert] mute jemalloc patch

--- contrib/jemalloc/include/jemalloc/jemalloc_FreeBSD.h
+++ contrib/jemalloc/include/jemalloc/jemalloc_FreeBSD.h
@@ -72,9 +72,9 @@
 #  define LG_SIZEOF_PTR		3
 #endif
 
-#if LG_VADDR > 32
-#  define JEMALLOC_RETAIN
-#endif
+// #if LG_VADDR > 32
+// #  define JEMALLOC_RETAIN
+// #endif
 
 #ifndef JEMALLOC_TLS_MODEL
 #  define JEMALLOC_TLS_MODEL	/* Default. */
